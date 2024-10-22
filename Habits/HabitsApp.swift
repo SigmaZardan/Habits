cf61415eb7a33e36f6e1dc6017a7fa0a569d6ae7
SigmaZardan <bibekbhujel077@gmail.com>
2024-10-22 12:59:52 +0545

    Initial Commit

---
+++ Habits/HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsApp.swift
+//  Habits
+//
+//  Created by Bibek Bhujel on 22/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabitsTests/HabitsTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsTests.swift
+//  HabitsTests
+//
+//  Created by Bibek Bhujel on 22/10/2024.
+//
+
+import Testing
+@testable import Habits
+
+struct HabitsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
